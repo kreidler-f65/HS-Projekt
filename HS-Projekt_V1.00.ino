4b97b34a74eb6f1af73e1921a5cbbae275a37ee0
Manuel Rude <manuelrude@gmail.com>
2015-02-04 19:34:48 +0100

    Ports berichtigt, Menü hinzugefügt

--- HS-Projekt_V1.00.ino
+++ HS-Projekt_V1.00.ino
@@ -1,9 +1,43 @@
+#include <EEPROM.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
 
+ //Pins setup
+ //Analog
+ const int STROM_MESS = 0;
+ const int BATT_SPG = 1;
+ //Digital
+ const int TAST1 = 23;
+ const int TAST2 = 25;
+ const int TAST3 = 27;
+ const int TAST4 = 29;
+ const int TAST5 = 31;
+ const int TAST6 = 33;
+ const int TAST7 = 35;
+ const int TAST8 = 37;
+ 
+ const int VOB = 39;
+ const int VOA = 42;
+ 
+ const int M_FORCE = 8;
+ 
+ const int SD_B = 7;
+ const int IN_B = 6;
+ const int SD_A = 5;
+ const int IN_A = 4;
+ const int INTER0 = 2;
+ const int INTER1 = 3;
+ const int INTER4 = 19;
+ const int INTER5 = 18;
+ 
+ 
+ //Variablen
+ char selected = '*';
 
-//Tastatur
+ 
+
+ //Tastatur
 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
@@ -14,62 +48,69 @@ char hexaKeys[ROWS][COLS] = {
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
-byte rowPins[ROWS] = {3, 2, 1, 0}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {7, 6, 5, 4}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {TAST1, TAST2, TAST3, TAST4}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {TAST5, TAST6, TAST7, TAST8}; //connect to the column pinouts of the keypad
 
 //initialize an instance of class NewKeypad
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
+
 //Display
-
-
 LiquidCrystal_I2C lcd(0x3F,20,4);  // set the LCD address to 0x27 for a 20 chars and 4 line display
 
- //Pins setup
- //Analog
- const int STROM_MESS = 97;
- const int BATT_SPG = 96;
- //Digital
- const int TAST1 = 77;
- const int TAST2 = 75;
- const int TAST3 = 73;
- const int TAST4 = 71;
- const int TAST5 = 59;
- const int TAST6 = 57;
- const int TAST7 = 55;
- const int TAST8 = 53;
- 
- const int VOB = 70;
- const int VOA = 51;
- 
- const int M_FORCE = 17;
- 
- const int SD_B = 16;
- const int IN_B = 15;
- const int SD_A = 5;
- const int IN_A = 1;
- const int INTER0 = 6;
- const int INTER1 = 7;
- const int INTER4 = 45;
- const int INTER5 = 46;
+
+//lcd.print("                    ");
 
 void setup() {
+  
  lcd.init(); 
  lcd.backlight();
  lcd.begin(20,4);
  lcd.clear();
- lcd.setCursor(0,0); //Start at character 0 on line 0
- lcd.print("Use Serial Mon");
+ //Startbildschirm
+ lcd.setCursor(0,0);
+ lcd.print("Handtrainer         ");
  lcd.setCursor(0,1);
- lcd.print("Type to display");
- lcd.setCursor(0,2); //Start at character 0 on line 0
- lcd.print("Use Serial Mon");
- lcd.setCursor(0,3);
- lcd.print("Type to display");
+ lcd.print("Version 1.00        ");
+ lcd.setCursor(0,2);
+ lcd.print("Manuel Rude         "); 
+ lcd.setCursor(19,3);
+ lcd.print("s");  
+ int count = 3;   //Anzeigezeit
+ while(count>=0){
+ lcd.setCursor(18,3);
+ lcd.print(count);
+ count--; 
+ delay(1000); 
+   }
+
+ Serial.begin(9600);
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+ switch(selected) {
+  case '*':
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Fahren:             ");
+  lcd.setCursor(0,3);
+  lcd.print("Einstellungen #     ");
+  break;
+  
+  case '#':
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Einstellungen:             ");
+  lcd.setCursor(0,3);
+  lcd.print("Fahren *            ");
+  break;
+  } 
+
+selected = customKeypad.getKey();
+delay(250);
+  
 
 }
